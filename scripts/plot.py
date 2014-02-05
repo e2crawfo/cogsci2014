f668b6fc1426b558e4eaa4975d0bf30afbd1b2a2
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-02-04 20:02:05 -0500

    Prettied up simulation plot some more.

--- scripts/plot.py
+++ scripts/plot.py
@@ -55,7 +55,7 @@ def load_simulation_data(fname, data):
 
 
 def simulation_plot(plot_fname, learning_fname='', testing_fname='', show=False):
-    mpl.rcParams['lines.linewidth'] = 0.7
+    mpl.rcParams['lines.linewidth'] = 1.0
 
     if not (learning_fname or testing_fname):
         print "Couldn't make plot, no filenames given"
@@ -69,8 +69,8 @@ def simulation_plot(plot_fname, learning_fname='', testing_fname='', show=False)
     if testing_fname:
         load_simulation_data(testing_fname, data)
 
-    num_plots = 5
-    fig = plt.figure()
+    num_plots = 2
+    fig = plt.figure(figsize=(6, 2.5))
 
     show_sims = 'vg' in data
     if show_sims:
@@ -86,24 +86,29 @@ def simulation_plot(plot_fname, learning_fname='', testing_fname='', show=False)
         stored_sim_funcs = [make_sim_func(vg.hrr_vectors[n]) for n in vg.G]
 
     offset = num_plots * 100 + 10 + 1
+    plt.subplots_adjust(right=0.93, top=0.98, bottom=0.11, left=0.07, hspace=0.01)
 
     t = data['t']
-
-    ax, offset = nengo_plot_helper(offset, t, data['address_input'], yticks=[])
-    ax, offset = nengo_plot_helper(offset, t, data['stored_input'], yticks=[])
-    ax, offset = nengo_plot_helper(offset, t, data['pre_decoded'], yticks=[])
-    ax, offset = nengo_plot_helper(offset, t, data['cleanup_spikes'], spikes=True, yticks=[])
-    ax, offset = nengo_plot_helper(offset, t, data['output_decoded'], yticks=[], removex=show_sims)
 
     if show_sims:
         address_sims = apply_funcs(address_sim_funcs, data['address_input'])
         ax, offset = nengo_plot_helper(offset, t, address_sims, yticks=[-1,0,1])
         plt.ylim((-1.1, 1.1))
+        plt.axhline(y = 1.0, color='black', linestyle='--', zorder=0)
 
         stored_sims = apply_funcs(stored_sim_funcs, data['output_decoded'])
         ax, offset = nengo_plot_helper(offset, t, stored_sims, yticks=[-1,0,1], removex=True)
         plt.ylim((-1.1, 1.1))
-        plt.axhline(y = 1.0, color='black', linestyle='--')
+        plt.axhline(y = 1.0, color='black', linestyle='--', zorder=0)
+
+    #ax, offset = nengo_plot_helper(offset, t, data['address_input'], yticks=[])
+    #ax, offset = nengo_plot_helper(offset, t, data['stored_input'], yticks=[])
+    #ax, offset = nengo_plot_helper(offset, t, data['pre_decoded'], yticks=[])
+    testing_time = data['testing_time'] * data['num_tests'] * 1000
+    cleanup_spikes = spike_sorter(data['cleanup_spikes'], data['num_vectors'] + 1,
+                        slice=np.index_exp[:-testing_time,:], binsize=20)
+    ax, offset = nengo_plot_helper(offset, t, cleanup_spikes, spikes=True, yticks=[])
+    ax, offset = nengo_plot_helper(offset, t, data['output_decoded'], yticks=[], removex=False)
 
     plt.savefig(plot_fname)
 
